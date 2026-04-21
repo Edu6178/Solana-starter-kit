85c6c99979c7d5be0d89367095c884fbe42675a5
Eduardo Uriel Pérez Rebollar <l202307053@vbravo.tecnm.mx>
2026-04-21 15:20:37 -0600

    Refactor user profile module and add functions

--- proyecto/src/lib.rs
+++ proyecto/src/lib.rs
@@ -1,18 +1,94 @@
 use anchor_lang::prelude::*;
 
-declare_id!("");
+declare_id!("HQVZgjeWRurdob47MnHxQMJcwSgeGUfgBaWzQhQ263ME");
 
 #[program]
-mod modulo {
+pub mod perfil_usuario {
     use super::*;
 
-    pub fn mi_primer_instruccion(ctx: Context<Saludo>) -> Result<()> {
-        msg!("Mi instruccion, funciona !!! :D");
+    pub fn crear_perfil(
+        ctx: Context<CrearPerfil>,
+        nombre: String,
+        biografia: String,
+    ) -> Result<()> {
+        let perfil = &mut ctx.accounts.perfil;
+
+        perfil.owner = ctx.accounts.owner.key();
+        perfil.nombre = nombre;
+        perfil.biografia = biografia;
+
+        msg!("Perfil creado con éxito!");
+        Ok(())
+    }
+
+    pub fn actualizar_biografia(
+        ctx: Context<ActualizarPerfil>,
+        nueva_biografia: String,
+    ) -> Result<()> {
+        let perfil = &mut ctx.accounts.perfil;
+
+        perfil.biografia = nueva_biografia;
+
+        msg!("Biografía actualizada!");
+        Ok(())
+    }
+
+    pub fn eliminar_perfil(_ctx: Context<EliminarPerfil>) -> Result<()> {
+        msg!("Perfil eliminado para siempre.");
         Ok(())
     }
 }
 
+#[derive(Accounts)]
+pub struct CrearPerfil<'info> {
+    #[account(
+        init, 
+        payer = owner, 
+        
+        space = 8 + 32 + (4 + 50) + (4 + 200), 
+        seeds = [b"perfil", owner.key().as_ref()], 
+        bump
+    )]
+    pub perfil: Account<'info, Perfil>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
-pub struct Saludo {}
+pub struct ActualizarPerfil<'info> {
+    #[account(
+        mut,
+        seeds = [b"perfil", owner.key().as_ref()], 
+        bump,
+        has_one = owner 
+    )]
+    pub perfil: Account<'info, Perfil>,
 
+    #[account(mut)]
+    pub owner: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct EliminarPerfil<'info> {
+    #[account(
+        mut,
+        close = owner, 
+        seeds = [b"perfil", owner.key().as_ref()], 
+        bump,
+        has_one = owner 
+    )]
+    pub perfil: Account<'info, Perfil>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+}
+
+#[account]
+pub struct Perfil {
+    pub owner: Pubkey,
+    pub nombre: String,
+    pub biografia: String,
+}
